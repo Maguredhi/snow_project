30f8df51303c3e53d8883d791ddc1b171b395ef5
maguredhi <genso1104@gmail.com>
2022-01-04 03:15:38 +0800

    add message and room MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/index'
+  get 'messages/new'
+  get 'messages/create'
+  resources :rooms do
+    resources :messages
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
